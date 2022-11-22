20f89990ee8763bfb0318c725c99cf0c3c5773cb
baronk2 <kevinbaron14@comcast.net>
2022-11-21 21:55:22 -0800

    Add data, structure Summary Paragraph

--- source/analysis.R
+++ source/analysis.R
@@ -3,6 +3,19 @@ library(tidyverse)
 # The functions might be useful for A4
 source("../source/a4-helpers.R")
 
+
+
+
+
+## Load data frame ---- 
+
+
+incarceration_trends <- read.csv(file = "C:/Users/Owner/Documents/Kevin/School/University_Of_Washington/Informatics/2022-2023/Autumn_2022/Info_201/info201/assignments/a4-baronk2/data/incarceration_trends.csv")
+# View(incarceration_trends)
+
+
+
+
 ## Test queries ----
 #----------------------------------------------------------------------------#
 # Simple queries for basic testing
@@ -20,25 +33,80 @@ test_query2 <- function(num=6) {
 
 ## Section 2  ---- 
 #----------------------------------------------------------------------------#
-# Your functions and variables might go here ... <todo: update comment>
+# Functions to calculate values referenced in Summary Paragraph of `index.Rmd`
 #----------------------------------------------------------------------------#
+
+get_total_US_prison_population_2017 <- function() {
+  
+  total_US_prison_population_2017 <- ""
+  
+  return(total_US_prison_population_2017)
+  
+}
+
+
+get_max_2017_prison_population_state <- function() {
+  
+  max_2017_prison_population_state <- ""
+  
+  return(max_2017_prison_population_state)
+  
+}
+
+
+get_max_state_2017_prison_population <- function() {
+  
+  max_state_2017_prison_population <- ""
+  
+  return(max_state_2017_prison_population)
+  
+}
+
+
+get_average_state_2017_prison_population <- function() {
+  
+  average_state_2017_prison_population <- ""
+  
+  return(average_state_2017_prison_population)
+  
+}
+
+
+get_black_incarceration_disproportionality_US_2017 <- function() {
+  
+  black_incarceration_disproportionality_US_2017 <- ""
+  
+  return(black_incarceration_disproportionality_US_2017)
+  
+}
+
 
 ## Section 3  ---- 
 #----------------------------------------------------------------------------#
 # Growth of the U.S. Prison Population
 # Your functions might go here ... <todo:  update comment>
 #----------------------------------------------------------------------------#
+
+
 # This function ... <todo:  update comment>
 get_year_jail_pop <- function() {
   # TODO: Implement this function 
 return()   
 }
 
+
 # This function ... <todo:  update comment>
 plot_jail_pop_for_us <- function()  {
   # TODO: Implement this function 
   return()   
 } 
+
+
+
+
+
+
+
 
 ## Section 4  ---- 
 #----------------------------------------------------------------------------#
@@ -47,6 +115,15 @@ plot_jail_pop_for_us <- function()  {
 # See Canvas
 #----------------------------------------------------------------------------#
 
+
+
+
+
+
+
+
+
+
 ## Section 5  ---- 
 #----------------------------------------------------------------------------#
 # <variable comparison that reveals potential patterns of inequality>
@@ -54,13 +131,17 @@ plot_jail_pop_for_us <- function()  {
 # See Canvas
 #----------------------------------------------------------------------------#
 
+
+
+
+
+
+
 ## Section 6  ---- 
 #----------------------------------------------------------------------------#
 # <a map shows potential patterns of inequality that vary geographically>
 # Your functions might go here ... <todo:  update comment>
 # See Canvas
 #----------------------------------------------------------------------------#
-
-## Load data frame ---- 
 
 
